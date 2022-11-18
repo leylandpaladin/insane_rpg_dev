be0128afc967cbbcfc7dda5e915587b79065b4e9
leylandpaladin <enwind123@gmail.com>
2022-11-18 16:59:54 +0300

    - Added simple camera switch preff "F" to third person view -All global variables are now in Globals.gd

--- Data/Scripts/Player.gd
+++ Data/Scripts/Player.gd
@@ -1,15 +1,11 @@
 extends KinematicBody
 # пример наследования, игрок это и есть кинематик боди.
-
+class_name PlayerControlsMain
 # Вектор3 это объект который передает данные о координатах игрока в 3д пространстве.
 # Обращаемся к велосити каждый раз когда нужно получить нашу позицию.
 var velocity = Vector3()
-
 # Гравитация - наш вес, в случае если нам понадобится падать
-var gravity = -30
-var max_speed = 16
-var jump_height = 70
-var mouse_sensitivity = 0.002
+
 # ! функция _ready выполняет вложенные в нее код когда объект инициалируется игрой.
 
 # ! SET MOUSE MODE Означает что игра захватывает мышь
@@ -22,6 +18,7 @@ func get_input():
 	# Трансформ - бейсиз, это координата Z от лица игрока, грубо говоря это сторона в которорую прямо сейчас смотрит игрок. 
 	# отнимая его Z мы движемся вперед! А прибавляя - назад.
 	# is_action_just_pressed принимает в себя название действия, бинд для него задается в настройках проекта godot.
+
 	if Input.is_action_pressed("move_forward"):
 		input_dir += -global_transform.basis.z
 	if Input.is_action_pressed("move_back"):
@@ -36,6 +33,15 @@ func get_input():
 	# Обработка Esc. Пока это сразу закрывает приложение, потом нужно будет открывать менюшку
 	if Input.is_action_pressed("ui_cancel"):
 		get_tree().quit()
+	if Input.is_action_just_pressed("change_cameramode"):
+		
+		if $Pivot/Camera_1st.current == true:
+			$Pivot/Camera_3rd/.set_current(true)
+			Globals.isInFirstPerson = false
+						
+		else:
+			$Pivot/Camera_1st.set_current(true)
+			Globals.isInFirstPerson = true
 	# Нормализирует инпут таким образом что мы не сможем слишком быстро стрейфится
 	input_dir = input_dir.normalized() 
 	
@@ -44,24 +50,26 @@ func get_input():
 		
 # Мауслук в этой функции
 func _unhandled_input(event):
+	
+
 	if event is InputEventMouseMotion:
 		# крутим башкой в 3д пространстве
 		# y лево/право
-		rotate_y(-event.relative.x * mouse_sensitivity)
+		rotate_y(-event.relative.x * Globals.mouse_sensitivity)
 		# Крутим башкой вверх-вниз с ограничением чтобы не крутится вертикальной центрифугой
-		$Pivot.rotate_x(-event.relative.y * mouse_sensitivity)
+		$Pivot.rotate_x(-event.relative.y * Globals.mouse_sensitivity)
 		$Pivot.rotation.x = clamp($Pivot.rotation.x, -1.2,1.2)
 		
 func _physics_process(delta):
 	
 	# получаем позицию нашего движения и множим на желаемый коэфициэнт скорость.
-	var desired_velocity = get_input() * max_speed
+	var desired_velocity = get_input() * Globals.player_max_speed
 	
 	# так как велосити - это экземпляр объекта Vector3, мы можем модифицировать вот таким вот образом:
 	# гравити * дельту сменит нашу позицию при падении. К примеру если захотим падать быстрее, можно менять вес.
 	# дельта - временной отрезок между предъидущим и текущим фреймом.
-	velocity.y += desired_velocity.y * jump_height * delta
-	velocity.y += gravity * delta
+	velocity.y += desired_velocity.y * Globals.player_jump_height * delta
+	velocity.y += Globals.player_gravity * delta
 	velocity.x = desired_velocity.x
 	velocity.z = desired_velocity.z
 
