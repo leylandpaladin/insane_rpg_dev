8b00fdf578dc3e063e5d42a8da8760792099fcf7
leylandpaladin <enwind123@gmail.com>
2022-11-18 17:04:44 +0300

    removed all old binaries + added missing files

---
+++ Data/Scripts/Globals.gd
@@ -0,0 +1,10 @@
+extends Node
+
+
+#Player Exclusive Variables:
+
+export var player_gravity = -30
+export var player_max_speed = 16
+export var player_jump_height = 70
+export var mouse_sensitivity = 0.002
+export var isInFirstPerson = true
