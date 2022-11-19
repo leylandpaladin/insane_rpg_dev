c7a3e5ef0c19222e77d472d5e1e4271cfe333170
leylandpaladin <enwind123@gmail.com>
2022-11-19 22:16:21 +0300

    added missing files

---
+++ Data/Scripts/DiaglogueLayer.gd
@@ -0,0 +1,29 @@
+extends CanvasLayer
+
+var player = null
+
+func _ready():
+	
+	var well = $"../DisgustingWell"
+	well.connect("interacted", self, "_on_Disgusting_well_interacted")
+	
+
+func _on_Disgusting_well_interacted(body):
+	print("signal recieved")
+	$DialogueBox.start()
+	Globals.mouseLocked = true
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	body.set_physics_process(false)
+	body.set_process_input(false)
+	player = body
+	
+
+
+func _on_DialogueBox_dialogue_ended():
+	Globals.mouseLocked = false
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	player.set_physics_process(true)
+	player.set_process_input(true)
+	player = null
+
+
