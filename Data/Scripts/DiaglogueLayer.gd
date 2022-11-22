1a6c5f5f13a3a600f4106e3858d3fe9d5cb87f1a
leylandpaladin <enwind123@gmail.com>
2022-11-22 21:21:42 +0300

    added dialogue signals handler abstraction, well now have fun effect

--- Data/Scripts/DiaglogueLayer.gd
+++ Data/Scripts/DiaglogueLayer.gd
@@ -3,14 +3,29 @@ extends CanvasLayer
 var player = null	
 class_name Dialogues
 var start_id = "1"
-
+var player_green = false
 onready var dialogueWindow = $DialogueBox
-
+onready var player_skin = $"../Appearance"
+onready var player_nose = $"../Pivot/Camera_1st/Nose"
+onready var player_test = $Player
 func _ready():
 	
 	SignalsGateway.connect("interacted", self, "on_Interaction")
+	dialogueWindow.connect("dialogue_signal", self, "dialogue_signal_handler")
 	print("signals loaded")
 	
+
+func dialogue_signal_handler(value):
+	
+	if value == "Opened":
+		get_tree().change_scene("res://Data/Scenes/BasementOfDoom.tscn")
+	elif value == "Well_used":		
+		
+		player_skin.get_surface_material(0).set_albedo(Color(0, 1, 0))
+		player_nose.get_surface_material(0).set_albedo(Color(0, 1, 0))
+	
+		
+
 func on_Interaction(body, target):	
 	
 	print("signal received from ", target, " to >>>> ", body)
@@ -59,9 +74,4 @@ func start_dialogue_file(target, start_id):
 	dialogueWindow.load_file("res://Data/Dialogues/%s.json" % [target.obj_name])
 	dialogueWindow.start(start_id)
 
-
-
-func _on_DialogueBox_dialogue_signal(Opened):
-	print("got ya")
-	get_tree().change_scene("res://Data/Scenes/BasementOfDoom.tscn")
 
