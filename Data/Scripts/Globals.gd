7238e23cf5b9cd2fb1509a9db9762c9602df7b00
leylandpaladin <enwind123@gmail.com>
2022-11-22 19:40:38 +0300

    fixed running bug in Leipzig_outskirts, added a triger to church door to test out something

--- Data/Scripts/Globals.gd
+++ Data/Scripts/Globals.gd
@@ -5,7 +5,7 @@ extends Node
 
 
 export var player_gravity = -30
-export var player_max_speed = 16
+export var player_max_speed = 32
 export var player_jump_height = 70
 export var mouse_sensitivity = 0.002
 export var isInFirstPerson = false
