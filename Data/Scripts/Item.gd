5927e2494603646217a3b16d0468127165045ca9
Narri <narri@hovinarri.localdomain>
2022-11-26 01:23:07 +0200

    Add base resource for items and update .gitignore

---
+++ Data/Scripts/Item.gd
@@ -0,0 +1,8 @@
+extends Resource
+
+export(int) var id = null
+export(String) var name = ""
+export(Enums.ItemType) var type = Enums.ItemType.Initial
+export(Image) var icon = null
+export(bool) var stackable = false
+export(int) var quantity = 1
