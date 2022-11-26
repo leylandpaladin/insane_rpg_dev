7583d4a5d61e9e991edb9ebac4cce317c64b890d
Narri <narri@hovinarri.localdomain>
2022-11-26 10:58:31 +0200

    Fix base item resource default values

--- Data/Scripts/Item.gd
+++ Data/Scripts/Item.gd
@@ -1,8 +1,8 @@
 extends Resource
+class_name default_item
 
-export(int) var id = null
-export(String) var name = ""
+export(String) var name = "" 
 export(Enums.ItemType) var type = Enums.ItemType.Initial
-export(Image) var icon = null
+export(Texture) var icon = null
 export(bool) var stackable = false
 export(int) var quantity = 1
