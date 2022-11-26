7a15683ebb6e9fbc3a0abd824ed264984e3a0c7c
Narri <narri@hovinarri.localdomain>
2022-11-26 11:03:17 +0200

    Add global inventory

---
+++ Data/Scripts/Inventory.gd
@@ -0,0 +1,32 @@
+extends Node
+
+signal items_changed(indexes)
+
+var columns := 9
+var rows := 3
+var slots := columns * rows
+var items = []
+
+func _ready():
+	for i in range(slots):
+		items.append({})
+		
+func set_item(index, item):
+	var previous_item = items[index]
+	items[index] = item
+	emit_signal("items_changed", [index])
+	return previous_item
+
+func remove_item(index):
+	var previous_item = items[index].duplicate()
+	items[index].clear()
+	emit_signal("items_changed", [index])
+	return previous_item
+	
+func set_item_quantity(index, amount):
+	items[index].quantity += amount
+	if items[index].quantity <= 0:
+		remove_item(index)
+	else:
+		emit_signal("items_changed", [index])
+	
